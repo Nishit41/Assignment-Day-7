6b793ce8d9894f66c27dd979277f33624f127fb4
Nishit <nishitranjan41@gmail.com>
2022-07-18 00:49:49 +0530

    this is about  UC -2 of tic tac toe

--- src/com/bridgelabz/basics/TicTacToe.java
+++ src/com/bridgelabz/basics/TicTacToe.java
@@ -2,15 +2,28 @@ package com.bridgelabz.basics;
 
 import java.util.Scanner;
 public class TicTacToe {
+    static char computerLetter;
+    static  char playerLetter;
         static char[] board =new char[10];
-        static void createBoard(char[]board){
-            for(int i=1;i<10;i++){
-                board[i]=' ';
+        static void createBoard(char[]board) {
+            for (int i = 1; i < 10; i++) {
+                board[i] = ' ';
             }
         }
+            static void chooseLetter(){
+                System.out.println("Choose any Letter (x,o)");
+                Scanner sc =new Scanner(System.in);
+                playerLetter=sc.next().charAt(0);
+                computerLetter=(playerLetter=='x')?'o':'x';
+                System.out.println("Your Letter is "+playerLetter);
+                System.out.println("Computer Letter is "+computerLetter);
+
+            }
+
         public static void main(String[] args) {
             createBoard(board);
             System.out.println("Welcome to tic tac toe");
+            chooseLetter();
         }
     }
 
