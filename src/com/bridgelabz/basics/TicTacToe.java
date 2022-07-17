dedc6a3f5f1ca47e7a336356741ecf1cfd6e48b0
Nishit <nishitranjan41@gmail.com>
2022-07-18 00:21:45 +0530

    this is about welcome UC -1 of tic tac toe

--- src/com/bridgelabz/basics/TicTacToe.java
+++ src/com/bridgelabz/basics/TicTacToe.java
@@ -1,14 +1,19 @@
 package com.bridgelabz.basics;
 
 import java.util.Scanner;
-
-
-    public class TicTacToe {
+public class TicTacToe {
+        static char[] board =new char[10];
+        static void createBoard(char[]board){
+            for(int i=1;i<10;i++){
+                board[i]=' ';
+            }
+        }
         public static void main(String[] args) {
+            createBoard(board);
             System.out.println("Welcome to tic tac toe");
         }
-
-
     }
+
+
 
 
