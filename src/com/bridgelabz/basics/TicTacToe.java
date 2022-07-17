87a78d9110f5618d2cefbfdd20fab5e478aa409e
Nishit <nishitranjan41@gmail.com>
2022-07-18 00:11:14 +0530

    this is about welcome msg of tic tac toe

---
+++ src/com/bridgelabz/basics/TicTacToe.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.basics;
+
+import java.util.Scanner;
+
+
+    public class TicTacToe {
+        public static void main(String[] args) {
+            System.out.println("Welcome to tic tac toe");
+        }
+
+
+    }
+
+
