63fab91825b05e9601f7c1349afb374e43f5d260
Nishit <nishitranjan41@gmail.com>
2022-07-18 00:59:06 +0530

    this is about  UC -3 of tic tac toe

--- src/com/bridgelabz/basics/TicTacToe.java
+++ src/com/bridgelabz/basics/TicTacToe.java
@@ -2,6 +2,7 @@ package com.bridgelabz.basics;
 
 import java.util.Scanner;
 public class TicTacToe {
+    static Scanner sc=new Scanner(System.in);
     static char computerLetter;
     static  char playerLetter;
         static char[] board =new char[10];
@@ -19,11 +20,17 @@ public class TicTacToe {
                 System.out.println("Computer Letter is "+computerLetter);
 
             }
+    static void showBoard(char[]board) {
+        System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+        System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
+        System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
+    }
 
         public static void main(String[] args) {
             createBoard(board);
             System.out.println("Welcome to tic tac toe");
             chooseLetter();
+            showBoard(board);
         }
     }
 
